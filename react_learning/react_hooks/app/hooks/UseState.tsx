9432308abfa613b84048f3d9c57a64c4bd56ff06
Chamith Chanuka <chamithroxmpc@gmail.com>
2026-07-19 22:43:31 +0530

    use state example

---
+++ react_learning/react_hooks/app/hooks/UseState.tsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+export default function UseState(){
+
+    const [count, setCount] = useState(0);
+
+    // handle increment
+    const handleIncrement = () => {
+        setCount((prev) => prev + 1)
+    }
+
+    // handle decrement
+    const handleDecrement = () => {
+        setCount((prev) => prev + 1)
+    }
+
+    const buttonStyle = "cursor-pointer px-2"
+
+    return (
+        <div>
+            <p>Count : {count}</p>
+            <button className={buttonStyle} onClick={handleIncrement}>Increase Counter</button>
+            <button className={buttonStyle} onClick={handleDecrement}>Decrease Counter</button>
+        </div>
+    )
+}
